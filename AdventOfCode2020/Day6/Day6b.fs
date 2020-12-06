42a4b905c4fb5d209151866d476fa980ecb3789f
marchelleboid <marc.helleboid@gmail.com>
2020-12-06 12:46:13 +0100

    Day 6 2020

---
+++ AdventOfCode2020/Day6/Day6b.fs
@@ -0,0 +1,34 @@
+﻿module Day6b
+
+open System.IO
+open System.Collections.Generic
+
+let handleLine (current : Dictionary<char, int>) (line : string) =
+    line.ToCharArray()
+        |> Array.iter (
+            fun x -> 
+            (
+                if current.ContainsKey(x) then
+                    let toAdd = current.[x] + 1
+                    current.Remove(x) |> ignore
+                    current.Add(x, toAdd)
+                else current.Add(x, 1))
+            );
+    current
+
+let finishGroup (group : Dictionary<char, int>) lineCount  =
+    group.Values |> Seq.filter (fun x -> x = lineCount) |> Seq.length
+
+let rec parseLines total current currentLineCount lines =
+    match lines with
+    | [] -> total + (finishGroup current currentLineCount)
+    | xs::rest -> 
+        match xs with
+        | "" -> parseLines (total + finishGroup current currentLineCount) (new Dictionary<char, int>()) 0 rest
+        | _ -> parseLines total (handleLine current xs) (currentLineCount + 1) rest
+
+let solver =
+    File.ReadLines "Inputs\\day6.txt"
+        |> List.ofSeq
+        |> parseLines 0 (new Dictionary<char, int>()) 0
+        |> printfn "%d"
