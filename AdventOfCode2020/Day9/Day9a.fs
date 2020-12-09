0ea7bd93939b18a02ce306ed65e6784c9de10e4c
marchelleboid <marc.helleboid@gmail.com>
2020-12-09 22:37:01 +0100

    Day 9 2020

---
+++ AdventOfCode2020/Day9/Day9a.fs
@@ -0,0 +1,20 @@
+﻿module Day9a
+
+open System.IO
+
+let rec isValueWeak value preamble =
+    match preamble with
+    | [] -> true
+    | xs::rest -> 
+        if List.contains (value - xs) rest then false
+        else isValueWeak value rest
+
+let rec findFirstWeakness index preamble (inputs : list<uint64>) =
+    let valueToCheck = inputs.[index]
+    if isValueWeak valueToCheck preamble then valueToCheck
+    else findFirstWeakness (index + 1) (valueToCheck :: preamble.[.. preamble.Length-1]) inputs
+
+let solver =
+    let inputs = File.ReadLines "Inputs\\day9.txt" |> Seq.map uint64 |> Seq.toList
+    let preamble = List.take 25 inputs
+    findFirstWeakness 0 preamble inputs.[25..] |> printfn "%d"
