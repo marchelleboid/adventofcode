8ca03b0791511eb20a9f513766a0a3ffc0636112
marchelleboid <marc.helleboid@gmail.com>
2020-12-11 22:23:22 +0100

    Day 10 2020

---
+++ AdventOfCode2020/Day10/Day10b.fs
@@ -0,0 +1,29 @@
+﻿module Day10b
+
+open System.IO
+
+let rec groupAdapters index current output adapters =
+    if index = List.length adapters then current :: output
+    else
+        if index = 0 then groupAdapters 1 [0] List.empty adapters
+        else 
+            let value = adapters.[index]
+            match value - adapters.[index - 1] with
+            | 3 -> groupAdapters (index + 1) [value] (current :: output) adapters
+            | _ -> groupAdapters (index + 1) (value :: current) output adapters
+
+let calculateSublistCombinations sublist =
+    match List.length sublist with
+    | 1 | 2 -> uint64 1
+    | 3 -> uint64 2
+    | 4 -> uint64 4
+    | 5 -> uint64 7
+    | _ -> printfn "ahh"; uint64 0
+
+let solver =
+    let adapters = File.ReadLines "Inputs\\day10.txt" |> Seq.map int |> Seq.sort |> Seq.toList
+    0 :: adapters
+        |> groupAdapters 0 List.empty List.empty
+        |> List.map calculateSublistCombinations
+        |> List.reduce (*)
+        |> printfn "%d"
