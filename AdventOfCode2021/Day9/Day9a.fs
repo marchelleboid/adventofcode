0492f8ad332af5e311eb623fdb8495bd1c23caa2
marchelleboid <marc.helleboid@gmail.com>
2021-12-09 22:57:20 +0100

    Day 9 2021

---
+++ AdventOfCode2021/Day9/Day9a.fs
@@ -0,0 +1,46 @@
+﻿module Day9a
+
+open System.IO
+
+let addAbove row column lines =
+    if row <> 0 
+    then [List.item column (List.item (row - 1) lines)]
+    else []
+
+let addBelow row column lines points =
+    if row <> (List.length lines - 1)
+    then List.item column (List.item (row + 1) lines) :: points
+    else points
+
+let addLeft column line points =
+    if column <> 0
+    then List.item (column - 1) line :: points
+    else points
+
+let addRight column line points =
+    if column <> (List.length line - 1)
+    then List.item (column + 1) line :: points
+    else points
+
+let isLowPoint row column height line lines =
+    let toCompare = 
+        addAbove row column lines
+        |> addBelow row column lines
+        |> addLeft column line
+        |> addRight column line
+    if List.forall (fun x -> height < x) toCompare
+    then height, true
+    else height, false     
+
+let solver =
+    let lines = 
+        File.ReadLines "Inputs\\day9.txt"
+            |> Seq.map (fun line -> Seq.map (fun c -> (int c - int '0')) line |> Seq.toList)
+            |> Seq.toList
+    lines
+        |> List.mapi 
+            (fun row line -> List.mapi (fun column height -> isLowPoint row column height line lines) line)
+        |> List.map (fun line -> List.filter (fun x -> snd x) line)
+        |> List.map (fun line -> List.map (fun v -> fst v + 1) line |> List.sum)
+        |> List.sum
+        |> printfn "%d"
---
+++ AdventOfCode2021/Day9/Day9b.fs
@@ -0,0 +1,33 @@
+﻿module Day9b
+
+open System.IO
+
+let rec countBasin x y (lines : int[][]) =
+    if x < 0 || x >= Array.length lines[0] || y < 0 || y >= (Array.length lines)
+    then 0
+    else if lines[y][x] = 9
+    then 0
+    else 
+    lines[y][x] <- 9;
+    1 + (countBasin (x + 1) y lines) + (countBasin (x - 1) y lines) + (countBasin x (y + 1) lines) + (countBasin x (y - 1) lines)
+
+let rec countBasins x y basins (lines : int[][]) =
+    if x >= Array.length lines[0]
+    then countBasins 0 (y + 1) basins lines
+    else if y >= (Array.length lines)
+    then basins
+    else if lines[y][x] = 9
+    then countBasins (x + 1) y basins lines
+    else
+        countBasins (x + 1) y ((countBasin x y lines) :: basins) lines
+
+let solver =
+    let lines = 
+        File.ReadLines "Inputs\\day9.txt"
+            |> Seq.map (fun line -> Seq.map (fun c -> (int c - int '0')) line |> Array.ofSeq)
+            |> Array.ofSeq
+    countBasins 0 0 [] lines
+        |> List.sortDescending
+        |> List.take 3
+        |> List.fold (fun accum x -> accum * x) 1
+        |> printfn "%d"
