4cde4181a75317c4e3bacd37cb64595fd9d31078
marchelleboid <marc.helleboid@gmail.com>
2021-12-10 20:33:00 +0100

    Day 10 2021

---
+++ AdventOfCode2021/Day10/Day10a.fs
@@ -0,0 +1,31 @@
+﻿module Day10a
+
+open System.IO
+
+let rec getErrorScore closureStack s =
+    match s with
+    | [] -> 0
+    | x :: xs ->
+        match x with
+        | '(' | '[' | '{' | '<' -> getErrorScore (x :: closureStack) xs
+        | ')' -> if List.head closureStack <> '('
+                 then 3
+                 else getErrorScore (List.tail closureStack) xs
+        | ']' -> if List.head closureStack <> '['
+                 then 57
+                 else getErrorScore (List.tail closureStack) xs
+        | '}' -> if List.head closureStack <> '{'
+                 then 1197
+                 else getErrorScore (List.tail closureStack) xs
+        | '>' -> if List.head closureStack <> '<'
+                 then 25137
+                 else getErrorScore (List.tail closureStack) xs
+        | _ -> getErrorScore closureStack xs // nope
+
+let solver =
+    File.ReadLines "Inputs\\day10.txt"
+        |> Seq.map Seq.toList
+        |> Seq.map (fun x -> getErrorScore [] x)
+        |> Seq.filter (fun x -> x <> 0)
+        |> Seq.sum
+        |> printfn "%d"
---
+++ AdventOfCode2021/Day10/Day10b.fs
@@ -0,0 +1,42 @@
+﻿module Day10b
+
+open System.IO
+
+let rec getErrorScore closureStack s =
+    match s with
+    | [] -> closureStack
+    | x :: xs ->
+        match x with
+        | '(' | '[' | '{' | '<' -> getErrorScore (x :: closureStack) xs
+        | ')' -> if List.head closureStack <> '('
+                 then []
+                 else getErrorScore (List.tail closureStack) xs
+        | ']' -> if List.head closureStack <> '['
+                 then []
+                 else getErrorScore (List.tail closureStack) xs
+        | '}' -> if List.head closureStack <> '{'
+                 then []
+                 else getErrorScore (List.tail closureStack) xs
+        | '>' -> if List.head closureStack <> '<'
+                 then []
+                 else getErrorScore (List.tail closureStack) xs
+        | _ -> getErrorScore closureStack xs // nope
+
+let rec calculateScore closureStack score =
+    match closureStack with
+    | [] -> score
+    | '(' :: xs -> calculateScore xs (score * 5UL + 1UL)
+    | '[' :: xs -> calculateScore xs (score * 5UL + 2UL)
+    | '{' :: xs -> calculateScore xs (score * 5UL + 3UL)
+    | '<' :: xs -> calculateScore xs (score * 5UL + 4UL)
+    | _ :: xs -> calculateScore xs score // nope
+
+let solver =
+    File.ReadLines "Inputs\\day10.txt"
+        |> Seq.map Seq.toList
+        |> Seq.map (fun x -> getErrorScore [] x)
+        |> Seq.filter (fun x -> not (List.isEmpty x))
+        |> Seq.map (fun x -> calculateScore x 0UL)
+        |> Seq.sort
+        |> fun x -> Seq.head (Seq.skip (Seq.length x / 2) x)
+        |> printfn "%d"
