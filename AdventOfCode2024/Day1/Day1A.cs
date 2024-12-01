ae60db9b040fd786a307adbc53394f9de04abd4d
marchelleboid <marc.helleboid@gmail.com>
2024-12-01 10:50:00 +0100

    Day 1 2024

---
+++ AdventOfCode2024/Day1/Day1A.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2024.Day1;
+
+public class Day1A : ISolver
+{
+    public void Solve()
+    {
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        
+        var lines = File.ReadLines("Inputs\\day1.txt");
+        foreach (var line in lines)
+        {
+            var splitLine = line.Split("   ");
+            list1.Add(int.Parse(splitLine[0]));
+            list2.Add(int.Parse(splitLine[1]));
+        }
+
+        list1.Sort();
+        list2.Sort();
+
+        Console.WriteLine(list1.Zip(list2, (a, b) => Math.Abs(a - b)).Sum());
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode2024;
+using AdventOfCode2024.Day1;
+
+ISolver solver = args[0] switch
+{
+    "1a" => new Day1A(),
+    "1b" => new Day1B(),
+    _ => throw new ArgumentException($"Unknown day: {args[0]}"),
+};
+
+solver.Solve();
